93708fd87131dd57e1ee1a2f69f91d367cf3c2b1
okozlovskyi <okozlovskyi@n-ix.com>
2024-09-30 09:51:49 +0300

    feat: test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const lambda = new LambdaClient({ region: 'us-east-1' });
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello2' });
+  res.status(200).json({ message: 'Hello' });
 });
 
 app.post('/tasks', (req, res) => {
