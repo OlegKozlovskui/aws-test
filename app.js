2e5dc82c49863a531040f7ea4645e44ff7f73164
okozlovskyi <okozlovskyi@n-ix.com>
2024-10-09 12:44:36 +0300

    feat: add google login

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 import multer from 'multer';
+import axios from 'axios';
 
 const app = express();
 const upload = multer();
 app.use(express.json());
+
+const lambda = new LambdaClient({ region: 'us-east-1' });
 
 // Додаткові опції CORS
 const corsOptions = {
@@ -17,8 +20,48 @@ const corsOptions = {
 // Використовуйте cors з опціями
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+const CLIENT_ID = '1u4fm24p0a1h89ne0ufqecgiip';
+const CLIENT_SECRET = '1mch48l7a8l3g7ochne15v4c71c7395u6k15th5i6v17orkakim8';
+const COGNITO_DOMAIN = 'https://taskmanager-app.auth.us-east-1.amazoncognito.com';
+const REDIRECT_URI = 'http://localhost:8080/callback'; // URL, на який Cognito перенаправляє після Google Sign-In
 
-const lambda = new LambdaClient({ region: 'us-east-1' });
+// Маршрут для перенаправлення на Google Sign-In через AWS Cognito
+app.get('/auth/google', (req, res) => {
+  const url = `${COGNITO_DOMAIN}/oauth2/authorize?identity_provider=Google&response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=email+openid+profile`;
+  res.redirect(url);
+});
+
+// Маршрут для обробки відповіді від Google і отримання токенів доступу
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    // Отримуємо токени від Cognito
+    const response = await axios.post(
+      `${COGNITO_DOMAIN}/oauth2/token`,
+      new URLSearchParams({
+        grant_type: 'authorization_code',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        code,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    const { id_token, access_token, refresh_token } = response.data;
+
+    // Повертаємо токени у відповіді або обробляємо їх далі
+    res.status(200).json({ id_token, access_token, refresh_token });
+  } catch (error) {
+    console.error('Error exchanging code for tokens:', error);
+    res.status(500).json({ error: 'Failed to exchange code for tokens' });
+  }
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello Cors' });
